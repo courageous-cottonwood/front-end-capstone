efd70971ea793dcf8e2f7be90e4f3a10cbc897de
Sean Welch <sean.welch@me.com>
2021-12-21 11:24:37 -0800

    modified server to serve react file

--- server/index.js
+++ server/index.js
@@ -1,10 +1,15 @@
+let path = require('path');
 let express = require('express');
 let app = express();
 
 let port = process.env.PORT || 3000;
 
+const staticPath = path.join(__dirname, '..', '/client/dist/');
+app.use(express.static(staticPath));
+app.use(express.json());
+
 app.listen(port);
 
 app.get('/', (req, res) => {
-  res.send('Hello World');
+  res.send('/index.html');
 })
