f796096b39d072c0ab50acca0d76417ee6eb6615
Sean Welch <sean.welch@me.com>
2021-12-21 11:14:35 -0800

    initial commit, webpack and babel configured for front-end

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+var path = require("path");
+var SRC_DIR = path.join(__dirname, "/client/src");
+var DIST_DIR = path.join(__dirname, "/client/dist");
+
+module.exports = {
+  mode: 'development',
+  devtool: 'eval-source-map',
+  watch: true,
+  entry: `${SRC_DIR}/index.jsx`,
+  output: {
+    filename: "bundle.js",
+    path: DIST_DIR,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)?/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: [
+              "@babel/preset-env",
+              "@babel/preset-react"
+            ]
+          }
+        },
+      },
+    ],
+  },
+};
