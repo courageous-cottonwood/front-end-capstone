5ef605a4f10b7f419a8ec7349f343ca8ffd695d8
Sean Welch <sean.welch@me.com>
2021-12-23 10:49:33 -0800

    added testing environment with enzyme for react-17

---
+++ client/src/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return(
+      <div>
+          <h1>Hello World</h1>
+      </div>
+    )
+  }
+}
+
+export default App;
