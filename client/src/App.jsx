0e6ed379881bce9a56c286a68b9c42c1e205b5cf
bgtdinh <bgtdinh@gmail.com>
2021-12-30 19:00:11 -0800

    Added Ratings and Reviews into merged app

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import QuestionsAnswers from './questions-answers/index.jsx';
+import Ratings_and_Reviews from './Ratings_and_Reviews/Ratings_and_Reviews.jsx';
 
 const App = () =>  {
 
