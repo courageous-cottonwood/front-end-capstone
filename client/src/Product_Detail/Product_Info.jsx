8dfeac1f50d3a8bfbf2bc12b311f8fbaba4bcf5f
Brandon Tsai <bjt888@gmail.com>
2022-01-06 15:09:50 -0800

    changed to purple stars

--- client/src/Product_Detail/Product_Info.jsx
+++ client/src/Product_Detail/Product_Info.jsx
@@ -37,7 +37,7 @@ const ProductInfo = (props) => {
       <Stars
         rating={getAverageRating()}
         size={20}
-        color='yellow'
+        color='#9484cd'
       />
       <h2>{props.product.name}</h2>
       <div>{props.product.description}</div>
